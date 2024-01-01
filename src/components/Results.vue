9899f73f3b8a6ad505f0696a93e445808356a38a
Burner09 <kurn_l_hector@hotmail.com>
2024-01-01 15:01:33 -0400

    test

---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Reaction time - {{ score }} ms</h2>
+  <h4 class="rank">{{ rank }}</h4>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers"
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail Pace...'
+    }
+  }
+
+}
+</script>
+
+<style>
+  .rank {
+    color: #0faf87;
+  }
+</style>
